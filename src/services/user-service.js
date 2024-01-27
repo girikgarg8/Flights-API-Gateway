22bdec7cbfbd129abf8e8e6aa7b3b9b1343d6e54
girikgarg8 <girikgarg8@gmail.com>
2024-01-27 16:46:55 +0530

    Feat: Add JWT expiry handling

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -51,16 +51,19 @@ async function isAuthenticated(token) {
             throw new AppError('Missing JWT Token', StatusCodes.BAD_REQUEST);
         }
         const response = Auth.verifyToken(token);
-        const user= await userRepository.get(response.id); //additional level of check, to ensure that the JWT token is not a stale token, and maybe the user was deleted from the table
-        if (!user){
-            throw new AppError('No user found',StatusCodes.NOT_FOUND);
+        const user = await userRepository.get(response.id); //additional level of check, to ensure that the JWT token is not a stale token, and maybe the user was deleted from the table
+        if (!user) {
+            throw new AppError('No user found', StatusCodes.NOT_FOUND);
         }
         return user.id;
     }
     catch (error) {
         if (error instanceof AppError) throw error;
         if (error.name == 'JsonWebTokenError') {
-            throw new AppError('Invalid JWT Token',StatusCodes.BAD_REQUEST);
+            throw new AppError('Invalid JWT Token', StatusCodes.BAD_REQUEST);
+        }
+        if (error.name == 'TokenExpiredError') {
+            throw new AppError('JWT Token Expired', StatusCodes.BAD_REQUEST);
         }
         console.log(error);
         throw new AppError('Something went wrong', StatusCodes.INTERNAL_SERVER_ERROR);
